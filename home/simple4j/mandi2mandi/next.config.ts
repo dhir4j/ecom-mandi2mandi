672245ea63a2a5f7ee561b2ff3cf8b685594b160
dhir4j <kapsedhiraj15@gmail.com>
2026-01-02 02:47:38 +0530

    Fix: Set correct build root directory

--- home/simple4j/mandi2mandi/next.config.ts
+++ home/simple4j/mandi2mandi/next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,6 +9,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  outputFileTracingRoot: path.join(__dirname),
   images: {
     remotePatterns: [
       {
